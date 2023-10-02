b8d76d56abe51271935286ff023258334651f02d
Kim Jiin <rlawldls1502@gmail.com>
2023-10-03 02:08:17 +0900

    알람 권한 수정 #1

--- a4s/lib/notification.dart
+++ a4s/lib/notification.dart
@@ -50,85 +50,38 @@ class FlutterLocalNotification {
   }
 
   static Future<void> showNotification() async {
+    // var scheduleNotificationDateTime =
+    // DateTime.now().add(Duration(seconds: 1));
+
+
     const AndroidNotificationDetails androidNotificationDetails =
-    AndroidNotificationDetails('channel id', 'channel name',
+    AndroidNotificationDetails(
+        'channel id',
+        'channel name',
         channelDescription: 'channel description',
+        // sound: RawResourceAndroidNotificationSound('a_long_cold_sting'),
+        // playSound: true,
         importance: Importance.max,
         priority: Priority.max,
-        showWhen: false);
+        showWhen: false
+    );
+
+    const DarwinNotificationDetails iosNotificationDetails =
+    DarwinNotificationDetails(
+        sound: 'a_long_cold_sting.wav',
+        presentAlert: true,
+        presentBadge: true,
+        presentSound: true);
 
     const NotificationDetails notificationDetails = NotificationDetails(
         android: androidNotificationDetails,
-        iOS: DarwinNotificationDetails(badgeNumber: 1));
+        iOS: iosNotificationDetails);
 
     await flutterLocalNotificationsPlugin.show(
-        0, 'test title', 'test body', notificationDetails);
+        0,
+        'test title',
+        'test body',
+        // scheduleNotificationDateTime,
+        notificationDetails);
   }
 }
-
-
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-//
-// class FlutterLocalNotification {
-//   FlutterLocalNotification._();
-//
-//   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-//   FlutterLocalNotificationsPlugin();
-//
-//   static init() async {
-//     AndroidInitializationSettings androidInitializationSettings =
-//     const AndroidInitializationSettings('mipmap/ic_launcher');
-//
-//     IOSInitializationSettings iosInitializationSettings = IOSInitializationSettings(
-//       requestAlertPermission: false,
-//       requestBadgePermission: false,
-//       requestSoundPermission: false,
-//       onDidReceiveLocalNotification:
-//         (int id, String? title, String? body, String? payload) async {});
-//
-//     InitializationSettings initializationSettings = InitializationSettings(
-//       android: androidInitializationSettings,
-//       iOS: iosInitializationSettings,
-//     );
-//
-//     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-//   }
-//
-//   static requestNotificationPermission() {
-//     flutterLocalNotificationsPlugin
-//         .resolvePlatformSpecificImplementation<
-//         IOSFlutterLocalNotificationsPlugin>()
-//         ?.requestPermissions(
-//       alert: true,
-//       badge: true,
-//       sound: true,
-//     );
-//   }
-//
-//   static Future<void> showNotification() async {
-//     const AndroidNotificationDetails androidNotificationDetails =
-//     AndroidNotificationDetails(
-//       'alarm_notif',
-//       'alarm_notif',
-//       'Channel for Alarm notification',
-//       icon: 'logo',
-//       sound: RawResourceAndroidNotificationSound('a_long_cold_sting'),
-//       largeIcon: DrawableResourceAndroidBitmap('logo'),
-//     );
-//
-//     const IOSNotificationDetails iosNotificationDetails =
-//     IOSNotificationDetails(
-//       sound: 'a_long_cold_sting.wav',
-//       presentAlert: true,
-//       presentBadge: true,
-//       presentSound: true
-//     );
-//
-//     const NotificationDetails notificationDetails = NotificationDetails(
-//         android: androidNotificationDetails,
-//         iOS: iosNotificationDetails);
-//
-//     await flutterLocalNotificationsPlugin.show(
-//         0, 'Office', 'Goodmorning', notificationDetails);
-//   }
-// }

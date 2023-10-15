c0a74eb11f82b894c940a4be082336a893817e54
Park-HY-00 <phy000115@naver.com>
2023-10-15 14:59:57 +0900

    utils, model 모듈화

--- a4s/model/model.py
+++ a4s/model/model.py
@@ -1,161 +1,55 @@
+from tensorflow.python.client import device_lib
+device_lib.list_local_devices()
+
+import torch
+print(torch.__version__)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('학습을 진행하는 기기:',device)
+
+import os
+import copy
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+from keras.layers import Input, Conv1D, BatchNormalization
+from keras.layers import Activation, MaxPool1D, GlobalAveragePooling1D
+from keras.layers import Dense, GaussianDropout, TimeDistributed
+from keras.models import Model
 
 import mne
 from mne.datasets.sleep_physionet.age import fetch_data
-from mne.time_frequency import psd_welch
+import model_utils
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer
+mne.set_log_level('ERROR')  # To avoid flooding the cell outputs with messages
 
-result_dir = './result/'
+subjects = range(30)
+recordings = [1]
 
-annotation_desc_2_event_id = {'Sleep stage W': 1,
-                              'Sleep stage 1': 2,
-                              'Sleep stage 2': 2,
-                              'Sleep stage 3': 3,
-                              'Sleep stage 4': 3,
-                              'Sleep stage R': 4}
+# # To load all subjects and recordings, uncomment the next line
+# subjects, recordings = range(83), [1, 2]
 
-# keep last 30-min wake events before sleep and first 30-min wake events after
-# sleep and redefine annotations on raw data
-annot_train.crop(annot_train[1]['onset'] - 30 * 60,
-                 annot_train[-2]['onset'] + 30 * 60)
-raw_train.set_annotations(annot_train, emit_warning=False)
+fnames = fetch_data(subjects=subjects, recording=recordings, on_missing='warn')
 
-events_train, _ = mne.events_from_annotations(
-    raw_train, event_id=annotation_desc_2_event_id, chunk_duration=30.)
+# Load recordings
+raws = [model_utils.load_sleep_physionet_raw(f[0], f[1]) for f in fnames]
 
-# create a new event_id that unifies stages 3 and 4
-event_id = {'Sleep stage W': 1,
-            'Sleep stage Light': 2,
-            'Sleep stage Deep': 3,
-            'Sleep stage R': 4}
+# Plot a recording as a sanity check
+# raws[0].plot();
 
-# plot events
-fig = mne.viz.plot_events(events_train, event_id=event_id,
-                          sfreq=raw_train.info['sfreq'],
-                          first_samp=events_train[0, 0])
+l_freq, h_freq = None, 30
 
-# keep the color-code for further plotting
-stage_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+for raw in raws:
+    raw.load_data().filter(l_freq, h_freq)  # filtering happens in-place
 
-tmax = 30. - 1. / raw_train.info['sfreq']  # tmax in included
+# # Plot the power spectrum of a recording as sanity check
+# raws[0].plot_psd();
 
-epochs_train = mne.Epochs(raw=raw_train, events=events_train,
-                          event_id=event_id, tmin=0., tmax=tmax, baseline=None)
+global event_id
 
-print(epochs_train)
-
-raw_test = mne.io.read_raw_edf(bob_files[0], stim_channel='Event marker',
-                               misc=['Temp rectal'])
-annot_test = mne.read_annotations(bob_files[1])
-annot_test.crop(annot_test[1]['onset'] - 30 * 60,
-                annot_test[-2]['onset'] + 30 * 60)
-raw_test.set_annotations(annot_test, emit_warning=False)
-events_test, _ = mne.events_from_annotations(
-    raw_test, event_id=annotation_desc_2_event_id, chunk_duration=30.)
-epochs_test = mne.Epochs(raw=raw_test, events=events_test, event_id=event_id,
-                         tmin=0., tmax=tmax, baseline=None)
-
-print(epochs_test)
-
-# visualize Alice vs. Bob PSD by sleep stage.
-fig, (ax1, ax2) = plt.subplots(ncols=2)
-
-# iterate over the subjects
-stages = sorted(event_id.keys())
-for ax, title, epochs in zip([ax1, ax2],
-                             ['Alice', 'Bob'],
-                             [epochs_train, epochs_test]):
-
-    for stage, color in zip(stages, stage_colors):
-        epochs[stage].plot_psd(area_mode=None, color=color, ax=ax,
-                               fmin=0.1, fmax=20., show=False,
-                               average=True, spatial_colors=False)
-    ax.set(title=title, xlabel='Frequency (Hz)')
-ax2.set(ylabel='µV^2/Hz (dB)')
-ax2.legend(ax2.lines[2::3], stages)
-plt.show()
-
-def eeg_power_band(epochs):
-    """EEG relative power band feature extraction.
-
-    This function takes an ``mne.Epochs`` object and creates EEG features based
-    on relative power in specific frequency bands that are compatible with
-    scikit-learn.
-
-    Parameters
-    ----------
-    epochs : Epochs
-        The data.
-
-    Returns
-    -------
-    X : numpy array of shape [n_samples, 5]
-        Transformed data.
-    """
-    # specific frequency bands
-    FREQ_BANDS = {"delta": [0.5, 4.5],
-                  "theta": [4.5, 8.5],
-                  "alpha": [8.5, 11.5],
-                  "sigma": [11.5, 15.5],
-                  "beta": [15.5, 30]}
-
-    psds, freqs = psd_welch(epochs, picks='eeg', fmin=0.5, fmax=30.)
-    # Normalize the PSDs
-    psds /= np.sum(psds, axis=-1, keepdims=True)
-
-    X = []
-    for fmin, fmax in FREQ_BANDS.values():
-        psds_band = psds[:, :, (freqs >= fmin) & (freqs < fmax)].mean(axis=-1)
-        X.append(psds_band.reshape(len(psds), -1))
-
-    return np.concatenate(X, axis=1)
-
-pipe = make_pipeline(FunctionTransformer(eeg_power_band, validate=False),
-                     RandomForestClassifier(n_estimators=100, random_state=42))
-
-# Train
-y_train = epochs_train.events[:, 2]
-pipe.fit(epochs_train, y_train)
-
-# Test
-y_pred = pipe.predict(epochs_test)
-
-# Assess the results
-y_test = epochs_test.events[:, 2]
-acc = accuracy_score(y_test, y_pred)
-
-print("Accuracy score: {}".format(acc))
-
-print(classification_report(y_test, y_pred, target_names=event_id.keys()))
-
-from sklearn.metrics import ConfusionMatrixDisplay
-cm = confusion_matrix(y_test, y_pred, labels=pipe.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
-disp.plot()
-plt.show()
-
-import seaborn as sns
-# Normalized confusion matrix
-cm = confusion_matrix(y_test, y_pred)
-cm_normalized = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis] #합이 1이 되록 각  데이터 스케일링
-
-# Plot confusion matrix
-fig, ax = plt.subplots(1)
-sns.heatmap(cm_normalized, annot=True, fmt='.2f', 
-xticklabels=event_id.keys(), yticklabels=event_id.keys(), cmap=plt.cm.Blues)
-ax.set(title='Normalized Confusion matrix')
-tick_marks = np.arange(len(event_id.keys()))
-plt.xticks(tick_marks, event_id.keys(), rotation=45)
-plt.yticks(tick_marks, event_id.keys())
-fig.tight_layout()
-ax.set(ylabel='True label', xlabel='Predicted label')
-
-plt.show()
-
+event_id = {
+        'Sleep stage W': 1,
+        'Sleep stage Light': 2,
+        'Sleep stage Deep': 3,
+        'Sleep stage R': 4}

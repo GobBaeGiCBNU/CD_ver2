67731216f92b96e0b26aa2fe31d5625b093eaa3e
Kim Jiin <rlawldls1502@gmail.com>
2023-10-15 20:48:05 +0900

    alarm range 추가 #2

--- a4s/lib/alarm/alarm.dart
+++ a4s/lib/alarm/alarm.dart
@@ -8,9 +8,10 @@ import 'package:a4s/service/alarm_scheduler.dart';
 import 'package:provider/provider.dart' as provider;
 import '../main.dart';
 
-
-void _createAlarm(BuildContext context,
-    AlarmListProvider alarmListProvider,) async {
+void _createAlarm(
+  BuildContext context,
+  AlarmListProvider alarmListProvider,
+) async {
   final time = await showTimePicker(
     context: context,
     initialTime: const TimeOfDay(hour: 8, minute: 30),
@@ -28,16 +29,15 @@ void _createAlarm(BuildContext context,
   await AlarmScheduler.scheduleRepeatable(alarm);
 }
 
-void _deleteAlarm(AlarmListProvider alarmListProvider,
-    Alarm alarm) async {
-
+void _deleteAlarm(AlarmListProvider alarmListProvider, Alarm alarm) async {
   alarmListProvider.remove(alarm);
-
 }
 
-void _switchAlarm(AlarmListProvider alarmListProvider,
-    Alarm alarm,
-    bool enabled,) async {
+void _switchAlarm(
+  AlarmListProvider alarmListProvider,
+  Alarm alarm,
+  bool enabled,
+) async {
   final newAlarm = alarm.copyWith(enabled: enabled);
   alarmListProvider.replace(
     alarm,
@@ -50,9 +50,11 @@ void _switchAlarm(AlarmListProvider alarmListProvider,
   }
 }
 
-void _handleCardTap(AlarmListProvider alarmList,
-    Alarm alarm,
-    BuildContext context,) async {
+void _handleCardTap(
+  AlarmListProvider alarmList,
+  Alarm alarm,
+  BuildContext context,
+) async {
   final time = await showTimePicker(
     context: context,
     initialTime: alarm.timeOfDay,
@@ -64,10 +66,16 @@ void _handleCardTap(AlarmListProvider alarmList,
   alarmList.replace(alarm, newAlarm);
   if (alarm.enabled) await AlarmScheduler.cancelRepeatable(alarm);
   if (newAlarm.enabled) await AlarmScheduler.scheduleRepeatable(newAlarm);
-
 }
 
-
+// Future<String> _rangeAlarm(Alarm alarm) async {
+//   var hour = alarm.timeOfDay.hour - 1;
+//   var min = alarm.timeOfDay.minute;
+//
+//   String range = hour.toString() + ':' + min.toString();
+//
+//   return range;
+// }
 
 class AlarmPage extends ConsumerStatefulWidget {
   const AlarmPage({super.key});
@@ -85,50 +93,52 @@ class _AlarmPage extends ConsumerState<AlarmPage> {
     });
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blueAccent,
-          onPressed: () {
-            _createAlarm(context, context.read<AlarmListProvider>());
-          },
-          child: const Icon(Icons.add, color: Colors.white,),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent,
+        onPressed: () {
+          _createAlarm(context, context.read<AlarmListProvider>());
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        body: Center(
-          child: Container(
-            child: Column(
-              children: [
-                Expanded(
-                  child: provider.Consumer<AlarmListProvider>(
-                    builder: (context, alarmList, child) =>
-                        ListView.builder(
-                          itemCount: alarmList.length,
-                          itemBuilder: (context, index) {
-                            final alarm = alarmList[index];
-                            return _AlarmCard(
-                              alarm: alarm,
-                              onTapSwitch: (enabled) {
-                                _switchAlarm(alarmList, alarm, enabled);
-                              },
-                              onTapCard: () {
-                                _handleCardTap(alarmList, alarm, context);
-                              },
-                              onTapDelete: () {
-                                _deleteAlarm(alarmList, alarm);
-                              },
-                            );
-                          },
-                        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: provider.Consumer<AlarmListProvider>(
+                  builder: (context, alarmList, child) => ListView.builder(
+                    itemCount: alarmList.length,
+                    itemBuilder: (context, index) {
+                      final alarm = alarmList[index];
+                      return _AlarmCard(
+                        alarm: alarm,
+                        onTapSwitch: (enabled) {
+                          _switchAlarm(alarmList, alarm, enabled);
+                        },
+                        onTapCard: () {
+                          _handleCardTap(alarmList, alarm, context);
+                        },
+                        onTapDelete: () {
+                          _deleteAlarm(alarmList, alarm);
+                        },
+                      );
+                    },
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
 
 class _AlarmCard extends StatelessWidget {
   const _AlarmCard({
@@ -150,7 +160,7 @@ class _AlarmCard extends StatelessWidget {
 
     return Card(
       margin: const EdgeInsets.all(16.0),
-      child: InkWell(
+      child: GestureDetector(
         onTap: onTapCard,
         child: Container(
           // 알람끼리 간격
@@ -162,52 +172,80 @@ class _AlarmCard extends StatelessWidget {
               gradient: LinearGradient(
                 colors: [
                   Color(0xFF856CFD).withOpacity(alarm.enabled ? 1.0 : 0.4),
-                  Color(0xff66a3ff).withOpacity(alarm.enabled ? 1.0 : 0.4)],
+                  Color(0xff66a3ff).withOpacity(alarm.enabled ? 1.0 : 0.4)
+                ],
                 begin: Alignment.centerLeft,
                 end: Alignment.centerRight,
               ),
-              borderRadius: BorderRadius.circular(24)
-          ),
-          child: InkWell(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        alarm.timeOfDay.format(context),
-                        style: theme.textTheme.headline6!.copyWith(
-                          fontSize: 26,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white.withOpacity(
-                            alarm.enabled ? 1.0 : 0.4,
-                          ),
+              borderRadius: BorderRadius.circular(24)),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      alarm.timeOfDay.format(context),
+                      style: theme.textTheme.headline6!.copyWith(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white.withOpacity(
+                          alarm.enabled ? 1.0 : 0.4,
                         ),
                       ),
                     ),
-                    Switch(
+                  ),
+                  Switch(
                       value: alarm.enabled,
                       onChanged: onTapSwitch,
-                      activeColor: Colors.white
+                      activeColor: Colors.white),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  // 알람 시간 범위 보여줌
+                  if (alarm.timeOfDay.hour - 1 < 0)
+                    Text(
+                      ('11:' + alarm.timeOfDay.minute.toString() + ' PM ~ ' +
+                          alarm.timeOfDay.format(context)),
+                      style: TextStyle(
+                          color: Colors.white
+                              .withOpacity(alarm.enabled ? 1.0 : 0.4),
+                          fontSize: 15),
+                    )
+                  else if (alarm.timeOfDay.hour - 1 > 12)
+                    Text(
+                      ((alarm.timeOfDay.hour - 13).toString() +
+                          ':' +
+                          alarm.timeOfDay.minute.toString() +
+                          ' ~ ' +
+                          alarm.timeOfDay.format(context)),
+                      style: TextStyle(
+                          color: Colors.white
+                              .withOpacity(alarm.enabled ? 1.0 : 0.4),
+                          fontSize: 15),
+                    )
+                  else
+                    Text(
+                      ((alarm.timeOfDay.hour - 1).toString() + ':' +
+                          alarm.timeOfDay.minute.toString() + ' ~ ' +
+                          alarm.timeOfDay.format(context)),
+                      style: TextStyle(
+                          color: Colors.white
+                              .withOpacity(alarm.enabled ? 1.0 : 0.4),
+                          fontSize: 15),
                     ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(""),
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        // IconButton를 누를 때 실행할 함수 또는 동작
-                        onTapDelete(); // onTapDelete 함수 실행
-                      },
-                      color: Colors.white,
-                    ),
-                  ],
-                )
-              ],
-            ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      // IconButton를 누를 때 실행할 함수 또는 동작
+                      onTapDelete(); // onTapDelete 함수 실행
+                    },
+                    color: Colors.white,
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),

a912bbc0705e4ffc4f94fbeb3fffa9e658437db7
Kim Jiin <rlawldls1502@gmail.com>
2023-10-30 12:20:40 +0900

    알람 후 설문조사 추가 #22

--- a4s/lib/alarm/alarm_screen.dart
+++ a4s/lib/alarm/alarm_screen.dart
@@ -4,6 +4,7 @@ import 'package:a4s/provider/alarm_state.dart';
 import 'package:a4s/service/alarm_scheduler.dart';
 import 'package:provider/provider.dart';
 import 'package:vibration/vibration.dart';
+import 'package:rating_dialog/rating_dialog.dart';
 import 'dart:async';
 
 class AlarmScreen extends StatefulWidget {
@@ -84,6 +85,7 @@ class _AlarmScreenState extends State<AlarmScreen> with WidgetsBindingObserver {
                   onPressed: () {
                     _dismissAlarm();
                     _vibrationTimer?.cancel();
+                    showAlertDialog();
                   },
                   child: const Text(
                     '중단',
@@ -98,4 +100,48 @@ class _AlarmScreenState extends State<AlarmScreen> with WidgetsBindingObserver {
       ),
     );
   }
+
+  void showAlertDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // set to false if you want to force a rating
+      builder: (context) => _dialog,
+    );
+  }
+
 }
+
+final _dialog = RatingDialog(
+  initialRating: 1.0,
+  // your app's name?
+  title: Text(
+    '오늘의 수면 평가',
+    textAlign: TextAlign.center,
+    style: const TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  // encourage your user to leave a high rating?
+  message: Text(
+    '수면 후 피로가 해소 되었나요?',
+    textAlign: TextAlign.center,
+    style: const TextStyle(fontSize: 15),
+  ),
+  // your app's logo?
+  // image: const FlutterLogo(size: 100),
+  submitButtonText: '제출',
+  commentHint: '불편 사항을 작성해 주세요.',
+  onCancelled: () => print('취소'),
+  onSubmitted: (response) {
+    print('rating: ${response.rating}, comment: ${response.comment}');
+
+    // TODO: add your own logic
+    // if (response.rating < 3.0) {
+    //   // send their comments to your email or anywhere you wish
+    //   // ask the user to contact you instead of leaving a bad review
+    // } else {
+    //   _rateAndReviewApp();
+    // }
+  },
+);
